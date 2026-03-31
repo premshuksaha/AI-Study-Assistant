67f0d15ee46a333a1dac2655ae77722f2a5a2050
premshuksaha <premshuk2020@gmail.com>
2026-03-31 20:27:48 +0530

    feat: add generateContent function to interact with Gemini API for content generation

---
+++ backend/services/gemini.service.js
@@ -0,0 +1,37 @@
+const Gemini_URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview:generateContent"
+
+export const generateContent = async (prompt) => {
+    try {
+        const response = await fetch(`${Gemini_URL}?key=${process.env.GEMINI_API_KEY}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                contents: [{
+                    parts: [
+                        { text: prompt }
+                    ]
+                }]
+            })
+        })
+        if (!response.ok) {        
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+
+        const text = data.candidates[0].content.parts[0].text;
+
+        if (!text) {
+            throw new Error('No text found in the response');
+        }
+        const cleanedText = text.replace(/```json/g, '')
+                                .replace(/```/g, '')
+                                .trim();
+
+        return JSON.parse(cleanedText);
+    } catch (error) {
+        console.error('Error generating content:', error);
+        throw error;
+    }
+}
